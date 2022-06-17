8abdf4f5dd721287a0bc32e9aa3b975437b3bdfa
vldmr_lev <vl.vl.levchenko@gmail.com>
2022-06-17 23:38:32 +0300

    Update reflink.js

--- index_files/reflink.js
+++ index_files/reflink.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
   $("a").click(function(){
-    $("#reflink*").attr("href", "https://onlinepugo.com/1bzlfx5p/?subId1=CSBR");
+    $("#reflink*").attr("href", "https://casinoonline777.com.br/rdrction/");
   });
 });
